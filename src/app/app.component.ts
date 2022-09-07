557b2a1786e8cbcbc2ad39ce0d9b944bc0331ff6
nevertheone <111437544+nevertheone@users.noreply.github.com>
2022-09-07 13:50:29 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, VERSION } from '@angular/core';
+import { TemplateUsageVisitor } from '@angular/core/schematics/migrations/static-queries/strategies/usage_strategy/template_usage_visitor';
 import { count } from 'rxjs';
 // Aufgabe 1: Schreibe fillTextUpToMaximum. Der Text aus "inputText" soll bis zur maximalen textlänge (variable lineMax mit fillUpSigns aufgefüllt werden)
 
@@ -7,9 +8,15 @@ import { count } from 'rxjs';
 // Aufgabe 3: Lasse den Text nach Leerzeichen für Leerzeichen nach rechts wandern und schreib jedes mal eine neue Zeile in textLines. Beispiel: Nimm das Leerzeichen rechts vom Text und füge es links vor dem Text an.
 // Aufgabe 4: Lasse den Text zeichen für Zeichen schieben
 
-// Aufgabe 5: Lasse 2/3 des Textes stehen, das letzte drittel Schiebst du FAST bis ganz nach links und lässt einen Buchstaben in der mitte 2x von links nach rechts wandern und wieder zurück
+// aufgabe 5: Ausgabe in Textfield
 
-// Aufgabe 6: Überleg dir etwas, wie der Text animiert aussieht. Gib als nicht alle Zeile aus, sondern nur immmer eine, damti es wirkt als wäre es eine Animation.
+// aufgabe 6: code aufräumen
+// - fillupsign verwenden
+// variablen und funktionen sprechend
+// code leserlicher machen und KISS
+
+//aufgabe 6
+// alles was bisher geschah, rückwärts
 
 @Component({
   selector: 'my-app',
@@ -18,18 +25,15 @@ import { count } from 'rxjs';
 })
 export class AppComponent {
   public lineMax: number = 40;
-  public fillUpSign: string = ' '; // Verwende dieses Zeichen um Aufzufüllen. Kein normales Leerzeichen
+  public fillUpSign: string = ' ';
   public inputTextfinalvalue: string;
   public inputText: string = 'Das ist mein Text';
   public textLines = [''];
 
-  constructor() {
-    //this.init();
-  }
   public submitinputvalues() {
     this.textLines = [''];
     if (this.inputText.length > this.lineMax) {
-      alert('beep');
+      alert('Error(15891): wabalaba dub dub');
     }
     this.init();
   }
@@ -49,44 +53,73 @@ export class AppComponent {
     this.inputTextfinalvalue = this.inputText;
     this.fillTextUpToMaximum();
     for (let i = 0; i < this.inputText.length; i++) {
-      this.movement();
+      this.SingleCharacterForwardMovement(false);
+    }
+    for (let i = 0; i < this.inputText.length; i++) {
+      this.SingleCharacterForwardMovement(true);
     }
   }
 
-  private movement() {
-    const countSpaces = this.inputTextfinalvalue.length - this.inputText.length;
-    const indexToInsert = this.inputTextfinalvalue.indexOf(' ') - 1;
-    console.log(indexToInsert);
-    for (let i = 0; i < countSpaces; i++) {
-      console.log('countspaces', countSpaces);
-      console.log('indexToInsert', indexToInsert);
-      this.inputTextfinalvalue =
-        this.inputTextfinalvalue.substring(0, indexToInsert) +
-        this.fillUpSign +
-        this.inputTextfinalvalue.substring(indexToInsert);
-      // if (i < countSpaces - 1) {
-      this.inputTextfinalvalue = this.cutSign(
-        this.inputTextfinalvalue,
-        indexToInsert,
-        i
-      );
-      //}
-
-      this.pushTextToLineEnd();
+  private SingleCharacterForwardMovement(isReverse: boolean) {
+    if (isReverse == false) {
+      const countSpaces =
+        this.inputTextfinalvalue.length - this.inputText.length;
+      const indexToInsert =
+        this.inputTextfinalvalue.indexOf(this.fillUpSign) - 1;
+      for (let i = 0; i <= countSpaces + 1; i++) {
+        this.inputTextfinalvalue =
+          this.inputTextfinalvalue.substring(0, indexToInsert) +
+          this.fillUpSign +
+          this.inputTextfinalvalue.substring(indexToInsert);
+        this.inputTextfinalvalue = this.cutSign(this.inputTextfinalvalue);
+        this.pushTextToLineEnd();
+      }
+    } else {
+      const countSpaces =
+        this.inputTextfinalvalue.length - this.inputText.length;
+      const indextoInsert =
+        this.inputTextfinalvalue.lastIndexOf(this.fillUpSign) + 2;
+      console.log(indextoInsert);
+      for (let i = 0; i <= countSpaces; i++) {
+        this.inputTextfinalvalue =
+          this.inputTextfinalvalue.substring(0, indextoInsert) +
+          this.fillUpSign +
+          this.inputTextfinalvalue.substring(indextoInsert);
+        this.inputTextfinalvalue = this.cutSignBackwards(
+          this.inputTextfinalvalue
+        );
+        this.pushTextToLineEnd();
+      }
     }
   }
 
-  private cutSign(input: string, index: number, numb: number): string {
-    console.log('cutSign');
-    console.log('input', input);
-    console.log("input.lastIndexOf(' ')", input.lastIndexOf(' '));
-    const indexOfLastSpace = input.lastIndexOf(' ');
-    console.log('indexOfLastSpace', indexOfLastSpace);
-    if (indexOfLastSpace !== -1)
+  private cutSign(input: string): string {
+    const indexOfLastSpace = input.lastIndexOf(this.fillUpSign);
+    if (indexOfLastSpace !== -1) {
+      if (input[indexOfLastSpace - 1] !== this.fillUpSign) {
+        return (
+          input.substring(0, indexOfLastSpace) +
+          input.substring(indexOfLastSpace + 1)
+        );
+      } else {
+        return (
+          input.substring(0, indexOfLastSpace - 1) +
+          input.substring(indexOfLastSpace)
+        );
+      }
+    } else {
+      return input;
+    }
+  }
+
+  private cutSignBackwards(input: string): string {
+    if (input.startsWith(this.fillUpSign)) {
       return (
-        input.substring(0, indexOfLastSpace - 1) +
-        input.substring(indexOfLastSpace)
+        input.substring(0, input.indexOf(this.fillUpSign) - 1) +
+        input.substring(input.indexOf(this.fillUpSign) + 1)
       );
-    else return input;
+    } else {
+      return input;
+    }
   }
 }
